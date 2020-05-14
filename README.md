6b93e73cca6c8788e0583bfda36ec5ec3064a17e
Oscar Gomez <corvstudios@gmail.com>
2020-05-14 06:52:51 -0500

    Added Car Italian Edition Meta

---
+++ README.md
@@ -0,0 +1,5 @@
+# FGR3D MetaZone Metas
+
+These are creations by FGR3D for distribution on MetaZone.
+
+* Car Italian Edition
--- src/game.ts
+++ src/game.ts
@@ -1,39 +1,16 @@
+import { CarItalianEdition } from '../metas/caritalianedition/caritalianedition'
 
-const _scene = new Entity('_scene')
-engine.addEntity(_scene)
-const transform = new Transform({
-  position: new Vector3(0, 0, 0),
-  rotation: new Quaternion(0, 0, 0, 1),
-  scale: new Vector3(1, 1, 1)
-})
-_scene.addComponentOrReplace(transform)
 
-const entity = new Entity('entity')
-engine.addEntity(entity)
-entity.setParent(_scene)
-const gltfShape = new GLTFShape("models/FloorBaseGrass_01/FloorBaseGrass_01.glb")
-gltfShape.withCollisions = true
-gltfShape.isPointerBlocker = true
-gltfShape.visible = true
-entity.addComponentOrReplace(gltfShape)
-const transform2 = new Transform({
-  position: new Vector3(8, 0, 8),
-  rotation: new Quaternion(0, 0, 0, 1),
-  scale: new Vector3(1, 1, 1)
-})
-entity.addComponentOrReplace(transform2)
+const caritalianeditionLandOwnerData = {
+  metadata: `
+  {
+    "meta": {
+      "position": {"x":8, "y":0, "z":8},
+      "rotation": {"x":0, "y":0, "z":0},
+      "scale": {"x":1, "y":1, "z":1}
+    }
+  }`
+}
 
-const fgrDITALYCAR = new Entity('fgrDITALYCAR')
-engine.addEntity(fgrDITALYCAR)
-fgrDITALYCAR.setParent(_scene)
-const transform3 = new Transform({
-  position: new Vector3(8, 0, 8),
-  rotation: new Quaternion(0, 0, 0, 1),
-  scale: new Vector3(1, 1, 1)
-})
-fgrDITALYCAR.addComponentOrReplace(transform3)
-const gltfShape2 = new GLTFShape("models/FGR3D-ITALY-CAR.glb")
-gltfShape2.withCollisions = true
-gltfShape2.isPointerBlocker = true
-gltfShape2.visible = true
-fgrDITALYCAR.addComponentOrReplace(gltfShape2)
+/// --- Set up your meta system to test ---
+engine.addSystem(new CarItalianEdition(null, caritalianeditionLandOwnerData))
